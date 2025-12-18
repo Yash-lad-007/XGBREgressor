4cf1201e08fe330a6361f48896822c8c521b5e84
Yash Lad <yashlad2812@gmail.com>
2025-12-18 10:13:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-import os
+import  os
 
 # Define file paths for the model and the data
 MODEL_FILE = "XGBRegressor.pkl"
